aa153e7a049693182124c5da53bf3beed982ea69
Gil Sunshine <gils@Gils-MacBook-Pro.local>
2020-03-23 12:15:56 -0400

    Initial build subtree commit

---
+++ build/precache-manifest.79e9eea216e3d440822dbdfa7d708c3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8697c4bcced3c394ce455c075419ed2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e7732141ab781917746",
+    "url": "/static/css/main.a2b7b0b6.chunk.css"
+  },
+  {
+    "revision": "b9971f3c1ffddf5736d9",
+    "url": "/static/js/2.d7dd0ead.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d7dd0ead.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e7732141ab781917746",
+    "url": "/static/js/main.0718e742.chunk.js"
+  },
+  {
+    "revision": "8ad77e743e472ba5fed2",
+    "url": "/static/js/runtime-main.7950a82b.js"
+  }
+]);
