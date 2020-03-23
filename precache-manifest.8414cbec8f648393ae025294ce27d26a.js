18d227e5ba07373251a5cc5538c854ccd0a254d2
Gil Sunshine <gils@Gils-MacBook-Pro.local>
2020-03-23 14:25:19 -0400

    Updates

---
+++ precache-manifest.8414cbec8f648393ae025294ce27d26a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "527a285d6eb78bc3d2b993ebbb0c3a1c",
+    "url": "/shackleton/index.html"
+  },
+  {
+    "revision": "c972260c516b0c1101b6",
+    "url": "/shackleton/static/css/main.a2b7b0b6.chunk.css"
+  },
+  {
+    "revision": "92d7e4125c461912430c",
+    "url": "/shackleton/static/js/2.2bad9646.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shackleton/static/js/2.2bad9646.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c972260c516b0c1101b6",
+    "url": "/shackleton/static/js/main.f90f7dab.chunk.js"
+  },
+  {
+    "revision": "f72458e1bb89bc304eef",
+    "url": "/shackleton/static/js/runtime-main.91970149.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ccc6ecb4cb55c23753612e10ddb1fa5a.js"
+  "/shackleton/precache-manifest.8414cbec8f648393ae025294ce27d26a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/shackleton/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
